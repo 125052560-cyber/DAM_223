9521b001cd547f08020493ace8f9c88ffb14823c
125052560-cyber <125052560@upq.edu.mx>
2026-09-25 11:58:18 -0600

    coffecode III
    actualizamos la caja

--- introjs/coffecode/caja.js
+++ introjs/coffecode/caja.js
@@ -1,53 +1,68 @@
-const menu = [
-  { producto: "Café Americano", precio: 2.50 },
-  { producto: "Café con Leche", precio: 3.00 },
-  { producto: "Café Espresso", precio: 2.00 },
-  { producto: "Café Capuchino", precio: 3.50 },
-  { producto: "Café Mocha", precio: 4.00 }
-];
+let listadePedidos = [];
+let totalAcumulado = 0;
 
-let pedidos = [];
+const cocina = require("./cocina.js")
 
-function destructuring(indice, callback) {
-  const { producto, precio } = menu[indice];
+function agregarPedido(producto, precio,cantidad) {
+  // se convierte en numero lo ingresado en terminal
+  const precioNum = Number(precio);
+  const cantidadNum = Number(cantidad);
+  const subtotal = precioNum * cantidadNum;
+  // se usa destructing aqui
+  listadePedidos.push({ producto, precio: precioNum, cantidad: cantidadNum, subtotal});
 
-  pedidos.push({ producto, precio, cantidad });
-
-  alert(`Elegiste: ${producto}\nPrecio: $${precio.toFixed(2)}`);
-
-  callback();
+  totalAcumulado += subtotal;
 }
 
-function mostrarMenu(callback) {
-  const opciones = menu
-    .map(({ producto, precio }, i) => `${i + 1}. ${producto} - $${precio.toFixed(2)}`)
-    .join("\n");
-
-  alert(`Menú de Café:\n${opciones}`);
-
-  callback();
+// Funciones para compartir los datos con el cliente.js
+function obtenerPedidos() {
+  return listadePedidos;
 }
 
-function caja() {
-  const subtotal = pedidos.reduce((total, { precio, cantidad }) => total + (precio * cantidad), 0);
+function obtenerTotal() {
+  return totalAcumulado;
+}
 
+/* PARTE DOS 
+
+destructing: {precio} en vez de poner objeto p y .precio
+
+*/
+
+
+function caja() { 
+  const subtotal = listadePedidos.reduce((acum, { subtotal }) => acum + subtotal, 0);
   const iva = subtotal * 0.16;
   const total = subtotal + iva;
 
-  const productos = pedidos
-    .map(({ producto, precio, cantidad }) => `- ${producto}: $${precio.toFixed(2)} (Cantidad: ${cantidad})`)
-    .join("\n");
-
-  alert(`Ticket de compra:\n\n${productos}\n\nSubtotal: $${subtotal.toFixed(2)}\nIVA (16%): $${iva.toFixed(2)}\nTotal a pagar: $${total.toFixed(2)}\nGracias por su compra\nNotificación: Pedido listo`);
+  const listaPedidos = listadePedidos.map(({ producto, precio, cantidad, subtotal }) => ({
+    producto,
+    precio,
+    cantidad,
+    subtotal
+  }));
+  
+  return {
+    listaPedidos,
+    subtotal,
+    iva,
+    total
+  }
 }
 
-mostrarMenu(() => {
-  const opcion = parseInt(prompt("Ingrese el número del café que desea:"), 10) - 1;
-  const cantidad = parseInt(prompt("Ingrese la cantidad que desea:"), 10);
+/* PARTE TRES: Marcar pedido realizado o rechazado*/
 
-  if (opcion >= 0 && opcion < menu.length) {
-    destructuring(opcion, () => caja());
-  } else {
-    alert("Opción inválida\nNotificación: Pedido cancelado");
-  }
-});
+function validarYProcesarPedido(nombreProducto, callback) {
+    console.log(`\nCaja: Consultando disponibilidad de '${nombreProducto}' con la cocina...`);
+    cocina.procesoCocina(nombreProducto)
+        .then((mensajeExito) => {
+          // primero parametro lee error, segundo exito
+            callback(null, `PEDIDO CONFIRMADO: ${mensajeExito}`);
+        })
+        .catch((mensajeError) => {
+            // Usamos el callback -> primer parámetro recibe el error
+            callback(`PEDIDO CANCELADO: ${mensajeError}`, null);
+        });
+}
+
+module.exports = {agregarPedido,obtenerPedidos,obtenerTotal,caja,validarYProcesarPedido};
